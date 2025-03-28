b77cb079c1b39d14806b2483ff726dbf40668db4
VIctor Wilson <vwilson05@gmail.com>
2025-03-28 15:55:14 -0700

    updated slack webhook

--- config/slack_config.py
+++ config/slack_config.py
@@ -6,7 +6,7 @@ def load_slack_config():
     In production, you might set this via secrets or environment variables.
     """
     # Replace the URL below with your actual Slack webhook URL.
-    os.environ["SLACK_WEBHOOK_URL"] = "https://hooks.slack.com/services/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"
+    os.environ["SLACK_WEBHOOK_URL"] = "https://hooks.slack.com/services/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"
     print("Slack webhook URL set.")
 
 if __name__ == "__main__":
